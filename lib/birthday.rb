3ce18a722880d586df6e62ce248ff528e51457a1
christine gomez <christine.gomez1226@gmail.com>
2018-09-07 21:00:20 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    yield birthday_kids
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
